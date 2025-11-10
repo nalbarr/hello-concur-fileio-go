f273e1b5a8ce9c83b9d4949e318c79b4e717c510
nalbarr <nalbarracin@gmail.com>
2025-11-09 19:49:50 -0600

    NA. Fix main.go to ensure all producer Goroutines complete.

--- main.go
+++ main.go
@@ -9,42 +9,22 @@ import (
 	"time"
 )
 
-func FileCloseCheck(f func() error) {
-	if err := f(); err != nil {
-		fmt.Println("Received error for file close.:", err)
-	}
-}
-
-func WriterFlushCheck(f func() error) {
-	if err := f(); err != nil {
-		fmt.Println("Received error for file writer flush..:", err)
-	}
-}
-
-func writer(id int, c <-chan int, filePath string, wg *sync.WaitGroup) {
+func writer(id int, c <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Printf("Goroutine %d start.\n", id)
-
+	fmt.Printf("Writer %d start.\n", id)
+	filePath := "ints.txt"
 	file, err := os.Create(filePath)
 	if err != nil {
 		fmt.Printf("Error creating file: %v\n", err)
 		return
 	}
-	// NA.
-	// - Remove linter warning
-	// defer file.Close()
-	// defer FileCloseCheck(file.Close())
 
 	writer := bufio.NewWriter(file)
-	// NA.
-	// - Remove linter warning
-	// defer writer.Flush()
-	// defer WriterFlushCheck(writer.Flush())
 
 	for x := range c {
 		idAsString := strconv.Itoa(id)
 		xAsString := strconv.Itoa(x)
-		fmt.Printf("Goroutine %d attempt to write: %s.\n", id, xAsString)
+		fmt.Printf("Writer %d attempt to write: %s.\n", id, xAsString)
 		_, err := writer.WriteString("goroutine " + idAsString + ", " + xAsString + "\n")
 		time.Sleep(time.Second)
 		if err != nil {
@@ -62,33 +42,50 @@ func writer(id int, c <-chan int, filePath string, wg *sync.WaitGroup) {
 	if err3 != nil {
 		fmt.Printf("Error during file close: %v\n", err)
 	}
-	fmt.Printf("Goroutine %d stop.\n", id)
+	fmt.Printf("Writer %d stop.\n", id)
+}
+
+func producer(id int, xs []int, c chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Producer %d start.\n", id)
+	for _, x := range xs {
+		fmt.Printf("Producer %d push: %d.\n", id, x)
+		c <- x
+		time.Sleep(time.Second)
+	}
+	fmt.Printf("Producer %d stop.\n", id)
 }
 
 func main() {
 	fmt.Println("Hello, concur ints and file io.")
 
-	filePath := "ints.txt"
-	xs := []int{7, 2, 8, -9, 4, 0}
-
 	// 1. Create channel for (native) ints
-	c := make(chan int)
-	var wg sync.WaitGroup
+	c := make(chan int, 100)
+	var wg0 sync.WaitGroup // 1x writer
+	var wg sync.WaitGroup  // 2x producers
 
-	// 2. Create 2 goroutines as file writers
-	for i := 1; i <= 3; i++ {
-		wg.Add(1) // increment goroutines count for wait group
-		go writer(i, c, filePath, &wg)
+	// 2. Create 1 file writer (will act as sync mutex)
+	wg0.Add(1) // increment goroutines count for wait group
+	go writer(1, c, &wg0)
+
+	// 3. Create 2 producers to push (native) ints to channel
+	xs := []int{7, 2, 8, -9, 4, 0}
+	numProducers := 2
+	for i := 0; i < numProducers; i++ {
+		wg.Add(1)
+		if i == 0 {
+			go producer(i, xs[:len(xs)/2], c, &wg)
+		} else {
+			go producer(i, xs[len(xs)/2:], c, &wg)
+		}
 	}
 
-	// 3. push (native) ints to channel, then close channel
-	for _, x := range xs {
-		c <- x
-	}
+	// 4. wait for all producers to finish
+	wg.Wait()
 	close(c)
 
-	// 5. wait for goroutines to finish
-	wg.Wait()
+	// 5. wait for writer to finish
+	wg0.Wait()
 
 	fmt.Println("Good bye.")
 }
